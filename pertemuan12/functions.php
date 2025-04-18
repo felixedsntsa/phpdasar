8396c4d27349706e273e29476d26c7cd4330189e
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-18 17:08:10 +0700

    membuat fitur upload gambar

---
+++ pertemuan12/functions.php
@@ -0,0 +1,135 @@
+<?php 
+
+// koneksi ke database
+$db = mysqli_connect("localhost","root","","phpdasar");
+
+// query function
+function query($query) {
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+// create function
+function tambah($data) {
+    global $db;
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    // upload gambar
+    $gambar = upload();
+    if(!$gambar ) {
+        return false;
+    }
+
+    $query = "INSERT INTO mahasiswa (nama, nim, email, jurusan, gambar)
+                VALUES 
+                ('$nama', '$nim', '$email', '$jurusan', 
+                '$gambar')
+                ";
+    mysqli_query($db,$query);
+
+    return mysqli_affected_rows($db);
+}
+
+// upload function
+function upload() {
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    // cek apakah tidak ada gambar yang diupload
+    if($error === 4) {
+        echo "<script>
+        alert('pilih gambar terlebih dahulu!'
+        </script>";
+        return false;
+    }
+
+    // cek apakah yang diupload adalah gambar
+    $ekstensiGambarValid = ['jpg','jpeg','png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if(!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo "<script>
+        alert('yang anda upload bukan gambar!'
+        </script>";
+        return false;
+    }
+
+    // cek jika ukurannya terlalu besar
+    if($ukuranFile > 2000000) {
+        echo "<script>
+        alert('gambar terlalu besar!'
+        </script>";
+        return false;
+    }
+
+    // lolos pengecekan, gambar siap diupload
+    // generate nama gambar baru
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ekstensiGambar;
+    move_uploaded_file($tmpName, '../src/img/' . $namaFileBaru);
+    return $namaFileBaru;
+
+}
+
+// delete function
+function hapus($id) {
+    global $db;
+    mysqli_query($db, "DELETE FROM mahasiswa WHERE id = $id");
+
+    return mysqli_affected_rows($db);
+}
+
+// update function
+function ubah($data) {
+    global $db;
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+
+    // cek apakah user pilih gambar baru atau tidak
+    if($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    } else {
+        $gambar= upload();
+    }
+
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "UPDATE mahasiswa SET
+                nama = '$nama',
+                nim = '$nim',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id";
+    mysqli_query($db,$query);
+
+    return mysqli_affected_rows($db);
+}
+
+// read function
+function cari($keyword) {
+    $query = "SELECT * FROM mahasiswa WHERE
+    nama LIKE '%$keyword%' OR
+    nim LIKE '%$keyword%' OR
+    email LIKE '%$keyword%' OR
+    jurusan LIKE '%$keyword%'";
+
+    return query($query);
+}
+
+?>
